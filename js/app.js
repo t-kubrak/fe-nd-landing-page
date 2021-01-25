4258771c7e00acdd788fc28adf2e443eced621da
tkubrak <Pre@36670>
2021-01-25 13:20:11 +0200

    feat: build the navigation menu

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@
  * Define Global Variables
  * 
 */
-
+const sectionNodes = document.querySelectorAll('section');
+const navbarList = document.querySelector('#navbar__list');
 
 /**
  * End Global Variables
@@ -34,7 +35,20 @@
 */
 
 // build the nav
+function buildNav(navbarList, sectionNodes) {
+    const fragment = document.createDocumentFragment();
 
+    for (sectionNode of sectionNodes) {
+        let listItem = document.createElement('li');
+        listItem.textContent = sectionNode.dataset.nav;
+        listItem.classList.add('menu__link');
+        fragment.appendChild(listItem);
+    }
+
+    navbarList.appendChild(fragment);
+}
+
+buildNav(navbarList, sectionNodes);
 
 // Add class 'active' to section when near top of viewport
 
